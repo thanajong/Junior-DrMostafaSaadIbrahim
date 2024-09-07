c10f5064f16c2f806a6a778307259fbabe9fffcd
Thanapat Jonglertthiti <thanajong@outlook.com>
2024-09-08 01:34:16 +0700

    update

---
+++ A/1-vanya-friends.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int n, h, width;
+int main()
+{
+    cin >> n >> h;
+    vector<int> f(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> f[i];
+    }
+    for (int i = 0; i < n; i++)
+        f[i] > h ? width += 2 : width++;
+    cout << width;
+}
