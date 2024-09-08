771037e619c88b18db8816e8e035fe7118221672
Thanapat Jonglertthiti <thanajong@outlook.com>
2024-09-08 19:09:21 +0700

    update

---
+++ A/2-anton-danik.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace  std;
+int g,antonCount,danikCount;
+string s,winner;       
+int main()
+{
+    cin >> g >> s;
+    for(int i=0; i<s.length(); i++){
+        if(s[i]=='A') antonCount++;
+        else if (s[i]=='D') danikCount++;
+        if(antonCount>s.length()/2) winner = "Anton";
+        else if(danikCount>s.length()/2) winner = "Danik";
+        else winner="Friendship";
+    }
+    cout<<winner;
+}
